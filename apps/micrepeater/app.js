073e7a7e7b0384ddf237d82fb0e4e214ed0adfef
Sjoerd Visscher <sjoerd@w3future.com>
2014-11-29 13:46:53 +0100

    follow audio

--- apps/micrepeater/app.js
+++ apps/micrepeater/app.js
@@ -1,27 +1,3 @@
-/*
-The MIT License (MIT)
-
-Copyright (c) 2014 Chris Wilson
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-*/
-
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 
 var audioContext = null;
@@ -133,11 +109,13 @@ function autoCorrelate( buf, sampleRate ) {
   for (var i=0;i<buf.length;i++)
     rms += buf[i]*buf[i];
   rms = Math.sqrt(rms/buf.length);
-  waveCanvas.strokeStyle = "blue";
-  waveCanvas.beginPath();
-  waveCanvas.moveTo(0,128);
+  // waveCanvas.strokeStyle = "blue";
+  // waveCanvas.beginPath();
+  // waveCanvas.moveTo(0,128);
 
-  for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset += 2) {
+  var prev = 0;
+  var find = false;
+  for (var offset = 0; offset <= MAX_SAMPLES; offset += 2) {
     var correlation = 0;
     var magnitude = 0;
 
@@ -149,85 +127,34 @@ function autoCorrelate( buf, sampleRate ) {
       magnitude += b0 * b0 + b1 * b1;
     }
     correlation = 2 * correlation / magnitude;
-    var fact = correlation / best_correlation;
-    var dist = Math.abs(fact - Math.round(fact));
-    if (correlation > 0.95 && correlation > best_correlation && (!best_correlation || dist > 0.05)) {
+    if (!find && prev < 0 && correlation > 0 && !best_correlation) {
+      find = true;
+    }
+    if (find && prev > 0 && correlation < 0)
+      break;
+    if (find && correlation > best_correlation) {
       best_correlation = correlation;
       best_offset = offset;
     }
-    waveCanvas.lineTo(offset-MIN_SAMPLES,128+(correlation*128));
+    prev = correlation;
+    // waveCanvas.lineTo(offset-MIN_SAMPLES,128+(correlation*128));
   }
-  waveCanvas.stroke();
-  waveCanvas.strokeStyle = "blue";
-  waveCanvas.beginPath();
-  waveCanvas.moveTo(best_offset - MIN_SAMPLES,0);
-  waveCanvas.lineTo(best_offset - MIN_SAMPLES,256);
-  waveCanvas.stroke();
-  if ((best_correlation > 0.9)) {
-    console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")");
+  // waveCanvas.stroke();
+  // waveCanvas.strokeStyle = "blue";
+  // waveCanvas.beginPath();
+  // waveCanvas.moveTo(best_offset - MIN_SAMPLES,0);
+  // waveCanvas.lineTo(best_offset - MIN_SAMPLES,256);
+  // waveCanvas.stroke();
+  if ((best_correlation > 0.7)) {
+    // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")");
     return sampleRate/best_offset;
   }
   return -1;
 }
 
-
-var MIN_SAMPLES = 5;  // will be initialized when AudioContext is created.
-
-function autoCorrelateInt( buf, sampleRate ) {
-  var SIZE = buf.length;
-  var MAX_SAMPLES = Math.floor(SIZE/2);
-  var best_offset = -1;
-  var best_correlation = 0;
-  var rms = 0;
-  var foundGoodCorrelation = false;
-  var correlations = new Array(MAX_SAMPLES);
-
-  for (var i=0;i<SIZE;i++) {
-    var val = buf[i];
-    rms += val*val;
-  }
-  rms = Math.sqrt(rms/SIZE);
-  if (rms<0.02) // not enough signal
-  return -1;
-
-  var lastCorrelation=1;
-  for (var offset = MIN_SAMPLES; offset < MAX_SAMPLES; offset++) {
-    var correlation = 0;
-
-    for (var i=0; i<MAX_SAMPLES; i++) {
-      correlation += Math.abs((buf[i])-(buf[i+offset]));
-    }
-    correlation = 1 - (correlation/MAX_SAMPLES);
-    correlations[offset] = correlation; // store it, for the tweaking we need to do below.
-    if ((correlation>0.01) && (correlation > lastCorrelation)) {
-      foundGoodCorrelation = true;
-      if (correlation > best_correlation) {
-        best_correlation = correlation;
-        best_offset = offset;
-      }
-    } /*else if (foundGoodCorrelation) {
-      // short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
-      // Now we need to tweak the offset - by interpolating between the values to the left and right of the
-      // best offset, and shifting it a bit.  This is complex, and HACKY in this code (happy to take PRs!) -
-      // we need to do a curve fit on correlations[] around best_offset in order to better determine precise
-      // (anti-aliased) offset.
-
-      // we know best_offset >=1,
-      // since foundGoodCorrelation cannot go to true until the second pass (offset=1), and
-      // we can't drop into this clause until the following pass (else if).
-      var shift = (correlations[best_offset+1] - correlations[best_offset-1])/correlations[best_offset];
-      return sampleRate/(best_offset+(8*shift));
-    }
-    lastCorrelation = correlation;*/
-  }
-  if (best_correlation > 0.01) {
-    // console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
-    return sampleRate/best_offset;
-  }
-  return -1;
-  //	var best_frequency = sampleRate/best_offset;
-}
-
+var hist = [];
+var lastNote;
+var socket;
 function updatePitch( time ) {
   var cycles = new Array;
   analyser.getFloatTimeDomainData( buf );
@@ -258,35 +185,40 @@ function updatePitch( time ) {
   }
 
   var ac = autoCorrelate( buf, audioContext.sampleRate );
+  var val = ac > -1 ? noteFromPitch(ac) % 12 : -1;
 
-  if (ac == -1) {
-    detectorElem.className = "vague";
-    pitchElem.innerText = "--";
-    noteElem.innerText = "-";
-    detuneElem.className = "";
-    detuneAmount.innerText = "--";
-  } else {
-    detectorElem.className = "confident";
-    pitch = ac;
-    pitchElem.innerText = Math.round( pitch ) ;
-    var note =  noteFromPitch( pitch );
-    noteElem.innerHTML = noteStrings[note%12];
-    var detune = centsOffFromPitch( pitch, note );
-    if (detune == 0 ) {
-      detuneElem.className = "";
-      detuneAmount.innerHTML = "--";
-    } else {
-      if (detune < 0)
-      detuneElem.className = "flat";
-      else
-      detuneElem.className = "sharp";
-      detuneAmount.innerHTML = Math.abs( detune );
+  hist.unshift(val);
+  var count = 0;
+  var SAMPLES = 15;
+  for (var i = 0; i < SAMPLES; i++)
+    if (hist[i] == val) count++;
+  if (count / SAMPLES > .8) {
+    if (lastNote != val) {
+      console.log(noteStrings[val]||"--");
+      if (!socket)
+        socket = io("http://10.42.35.16:9001")
+      if (val > -1)
+        socket.emit('url', '/xylofoon/' + ((val + 7)%12))
+      if (val == -1) {
+        detectorElem.className = "vague";
+        pitchElem.innerText = "--";
+        noteElem.innerText = "-";
+        detuneElem.className = "";
+        detuneAmount.innerText = "--";
+      } else {
+        detectorElem.className = "confident";
+        noteElem.innerHTML = noteStrings[val];
+        detuneElem.className = "";
+        detuneAmount.innerHTML = "--";
+      }
     }
+    lastNote = val;
   }
 
-  if (!window.requestAnimationFrame)
+  /*if (!window.requestAnimationFrame)
   window.requestAnimationFrame = window.webkitRequestAnimationFrame;
-  rafID = window.requestAnimationFrame( updatePitch );
+  rafID = window.requestAnimationFrame( updatePitch );*/
+  setTimeout(updatePitch, 0)
 }
 
 toggleLiveInput()
